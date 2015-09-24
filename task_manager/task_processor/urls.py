8c2cefd64e1e0c03a3d4cdedcacd539182ef8c10
geekan <geekan@foxmail.com>
2015-09-24 15:36:55 +0800

    fix

--- task_manager/task_processor/urls.py
+++ task_manager/task_processor/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^add/(.*)/?$', views.add_task, name='add_task'),
-    url(r'^neural-task/(.*)/?$', views.add_task),
+    url(r'^add/(.*)/?$', views.neural_task, name='add_task'),
+    url(r'^neural-task/(.*)/?$', views.neural_task),
 ]
