031171b346118171563aa369046ce03c41d8d939
geekan <geekan@foxmail.com>
2015-09-24 23:20:56 +0800

    use timezone.

--- task_manager/task_processor/views.py
+++ task_manager/task_processor/views.py
@@ -11,13 +11,14 @@ import logging
 l = logging.getLogger(__name__)
 
 def index(request):
+    #l.error(__name__)
     tasks = list(ImageNeuralTask.objects.all().values())
-    l.debug(tasks)
+    #l.debug(tasks)
     return HttpResponse('<br/>'.join([str(task) for task in tasks]))
 
 @csrf_exempt
 def neural_task(request, *args, **kwargs):
-    l.info(args, kwargs, request.POST, request.GET)
+    l.warn(args, kwargs, request.POST, request.GET)
     good_paras = ['image_url', 'image_id', 'style_image_path', 'user_id']
     para_dict = {k: request.REQUEST.get(k, '') for k in good_paras}
     para_dict['create_time'] = strftime("%Y-%m-%d %H:%M:%S", localtime())
@@ -28,6 +29,6 @@ def neural_task(request, *args, **kwargs):
 
 @csrf_exempt
 def neural_task_clean(request, *args, **kwargs):
-    l.info(args, kwargs, request.POST, request.GET)
+    l.warn(args, kwargs, request.POST, request.GET)
     ImageNeuralTask.objects.filter(Q(image_id='') | Q(user_id='')).delete()
     return index(request)
